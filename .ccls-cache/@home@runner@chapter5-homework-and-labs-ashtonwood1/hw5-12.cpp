15c504248a439c73f70c384c19f623fb8203aa8d
ashtonwood1 <ashtonhughwood@outlook.com>
2024-10-18 01:06:17 +0000

    hw5-12
    hw5-12

--- .ccls-cache/@home@runner@chapter5-homework-and-labs-ashtonwood1/hw5-12.cpp
+++ .ccls-cache/@home@runner@chapter5-homework-and-labs-ashtonwood1/hw5-12.cpp
@@ -7,5 +7,30 @@ using namespace std;
 
 int main()
 {
-	
+  double price1;
+  double price2;
+  double discount; 
+  double total_cost;
+
+  
+  cout << "Price of the first item: ";
+  cin >> price1;
+  cout << "Price of the second item: ";
+  cin >> price2;
+
+  if (price1 < price2) {
+    discount = price1 * 0.5; 
+  } else if (price2 < price1) {
+    discount = price2 * 0.5; 
+  } else { 
+    discount = price2 * 0.5;
+  }
+
+  total_cost = price1 + price2 - discount;
+  cout << fixed << setprecision(2); 
+  cout << "Total cost: " << total_cost << endl;
+
+  return 0;
+
+  
 }	//end of main function
